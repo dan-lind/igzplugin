4439f2083a8529cca24c702bf513f18e5447ad29
Daniel Lindberg <daniel.lindberg@omegapoint.se>
2017-05-26 14:09:44 +0200

    Improved connection stability, added retry for confirmation, add delay for confirmation

---
+++ src/main/java/com/danlind/igz/misc/RetryWithDelay.java
@@ -0,0 +1,37 @@
+package com.danlind.igz.misc;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
+import java.util.concurrent.TimeUnit;
+
+public class RetryWithDelay implements Function<Observable<? extends Throwable>, Observable<?>> {
+    private final int maxRetries;
+    private final int retryDelayMillis;
+    private int retryCount;
+
+    public RetryWithDelay(final int maxRetries, final int retryDelayMillis) {
+        this.maxRetries = maxRetries;
+        this.retryDelayMillis = retryDelayMillis;
+        this.retryCount = 0;
+    }
+
+    @Override
+    public Observable<?> apply(final Observable<? extends Throwable> attempts) {
+        return attempts
+            .flatMap(new Function<Throwable, Observable<?>>() {
+                @Override
+                public Observable<?> apply(final Throwable throwable) {
+                    if (++retryCount < maxRetries) {
+                        // When this Observable calls onNext, the original
+                        // Observable will be retried (i.e. re-subscribed).
+                        return Observable.timer(retryDelayMillis,
+                            TimeUnit.MILLISECONDS);
+                    }
+
+                    // Max retries hit. Just pass the error along.
+                    return Observable.error(throwable);
+                }
+            });
+    }
+}
